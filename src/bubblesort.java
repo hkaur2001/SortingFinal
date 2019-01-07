421229a1bc4e49f6e1324d53f494d7256f34017e
BT_1N3_28@BKTECH.LOCAL <BT_1N3_28@BKTECH.LOCAL>
2019-01-07 14:46:24 -0500

    Initial commit

---
+++ src/bubblesort.java
@@ -0,0 +1,30 @@
+public class bubblesort
+{
+
+    public static int [] bubble (int [] in)
+    {
+        int [] out = in;
+        boolean unsorted = true;
+        boolean hasChanged;
+        while (unsorted)
+        {
+            hasChanged = false;
+            for (int i = 0; i < out.length -1;i++)
+            {
+                if (out[i] > out[i+1])
+                {
+                    sortingUtil.swap(out,i,i+1);
+                    hasChanged = true;
+                }
+            }
+            if (!hasChanged)
+            {
+                unsorted = false;
+            }
+        }
+
+        return out;
+    }
+
+
+}
